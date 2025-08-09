4d247c686f1a20cebac7b315f2d2b40a98a92fde
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-08-09 17:37:39 -0400

    refactor(i18n): reorganize JSON

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -2,21 +2,21 @@ import { useTranslations } from 'next-intl';
 import {Link} from '@/i18n/navigation';
 
 const Footer = () => {
-    const t = useTranslations('LandingPage');
+    const t = useTranslations('LandingPage.footer');
 
     return (
         <footer className="bg-[#161b22] text-white/50 py-10 px-6 border-t border-white/10 w-full">
             <div className="grid grid-cols-1 sm:grid-cols-4 gap-6 text-sm max-w-full mx-auto">
                 <div>
                     <h4 className="font-semibold text-white mb-2">OpenCodeX</h4>
-                    <p>{t('headline_footer')}</p><p className='text-[#161b22]'>this is a easter egg for solution a bug..........................................................</p>
+                    <p>{t('title')}</p><p className='text-[#161b22]'>this is a easter egg for solution a bug..........................................................</p>
                 </div>
                 <div>
                     <h4 className="font-semibold text-white mb-2">{t('platform')}</h4>
                     <ul className="space-y-1">
-                        <li><Link href="#">{t('button_explore_projects')}</Link></li>
-                        <li><Link href="#">{t('nav_developers')}</Link></li>
-                        <li><Link href="#">{t('create_project')}</Link></li>
+                        <li><Link href="#">{t('button.projects')}</Link></li>
+                        <li><Link href="#">{t('button.developers')}</Link></li>
+                        <li><Link href="#">{t('button.createProject')}</Link></li>
                     </ul>
                 </div>
                 <div>
@@ -30,9 +30,9 @@ const Footer = () => {
                 <div>
                     <h4 className="font-semibold text-white mb-2">{t('support')}</h4>
                     <ul className="space-y-1">
-                        <li><Link href="#">{t('help_center')}</Link></li>
-                        <li><Link href="#">{t('privacity')}</Link></li>
-                        <li><Link href="#">{t('contact')}</Link></li>
+                        <li><Link href="#">{t('button.helpCenter')}</Link></li>
+                        <li><Link href="#">{t('button.privacity')}</Link></li>
+                        <li><Link href="#">{t('button.contact')}</Link></li>
                     </ul>
                 </div>
             </div>
--- components/landing/Stats.tsx
+++ components/landing/Stats.tsx
@@ -1,21 +1,21 @@
 import { useTranslations } from 'next-intl';
 
 const Stats = () => {
-    const t = useTranslations('LandingPage');
+    const t = useTranslations('LandingPage.stats');
 
     return (
         <section className="flex justify-center sm:gap-20 gap-8 py-8 border-y border-white/10 text-center text-lg bg-[#161b22]">
             <div>
                 <p className="text-purple-500 font-bold text-2xl">1.024</p>
-                <span>{t('stat_projects')}</span>
+                <span>{t('projects')}</span>
             </div>
             <div>
                 <p className="text-purple-500 font-bold text-2xl">2.230</p>
-                <span>{t('stat_developers')}</span>
+                <span>{t('developers')}</span>
             </div>
             <div>
                 <p className="text-purple-500 font-bold text-2xl">4.096</p>
-                <span>{t('stat_collaborations')}</span>
+                <span>{t('collaborations')}</span>
             </div>
         </section>
     )
