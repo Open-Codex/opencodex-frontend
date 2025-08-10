1e19b12fcad01f288c92fd2afbdb156c5431b9b9
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-08-10 00:13:27 -0400

    refactor(fetch): change text error

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -2,6 +2,6 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function apiFetch(path: string, options?: RequestInit) {
     const res = await fetch(`${API_URL}${path}`, options);
-    if (!res.ok) throw new Error('Error al hacer fetch');
-    return res.json();
+    if (!res.ok) throw new Error('Error to fetch');
+    return res;
 }
